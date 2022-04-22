1e220aa7e27e3d68255c808e4751ab5b70a03fb7
hrnoksz <harun1973@yahoo.com>
2022-04-22 10:35:32 +0300

    added more work

--- src/work1/Hello.java
+++ src/work1/Hello.java
@@ -7,5 +7,7 @@ public class Hello {
         System.out.println("Work1 started");
         System.out.println("Created Git Repository");
         System.out.println("Made few commits");
+        System.out.println("Local Repo is already published to GitHub");
+        System.out.println("Doing more work");
     }
 }
